ebf1304ccf6e86272877992bdb7ee44bb94db630
qooktree1 <qooktree@gmail.com>
2022-07-08 21:13:10 +0900

    naver

--- react-frontend/src/components/Kakao/Kakao.js
+++ react-frontend/src/components/Kakao/Kakao.js
@@ -4,7 +4,8 @@ import axios from 'axios'
 
 export default function Kakao() {
   const REST_API_KEY = process.env.REACT_APP_KAKAO_API_KEY
-  const REDIRECT_URI = "https://whatisyourword.netlify.app/oauth/kakao/callback"
+  // const REDIRECT_URI = "https://whatisyourword.netlify.app/oauth/kakao/callback"
+  const REDIRECT_URI = "https://www.naver.com"
   const LOGIN_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code`
   return (
     <div>
