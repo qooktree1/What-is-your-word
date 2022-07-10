793aa1b582a85c277cafccfb6c3614f809210ea1
qooktree1 <qooktree@gmail.com>
2022-07-10 14:01:05 +0900

    urls fixed

--- Django_backend/accounts/urls.py
+++ Django_backend/accounts/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('api/v1/kakao/login/', views.kakao_get_login),
-    path('api/v1/kakao/logout/', views.kakao_logout),
-    path('api/v1/kakao/redirect/', views.kakao_redirect),
+    path('kakao/login/', views.kakao_get_login),
+    path('kakao/logout/', views.kakao_logout),
+    path('kakao/redirect/', views.kakao_redirect),
 ]
